b15c333b5611eaf7c09925ce39697a6a5a849c8c
Dolphman <therealdolphman@gmail.com>
2016-01-14 14:51:33 -0500

    Added RateLimit Object

--- wethepeople/core/__init__.py
+++ wethepeople/core/__init__.py
@@ -1,5 +1,9 @@
 from requests import Session
-from . import exceptions
+from time import time as timestamp
+#from . import exceptions
+
+__rltracker__ = list()
+
 
 
 def ErrorProbe(response):
@@ -23,7 +27,53 @@ def ErrorProbe(response):
             ))
 
 
-class SessionWrapper(object):
+class RateLimit(object):
+
+    """
+    This object wraps around the ratelimiting system
+
+    Classes that use the rate-limiter must inherit this.
+
+    If a function needs to use the rate limiter, it must create
+    a RateLimit() obj and use its methods. This protect the
+    global state of the Rate Limiter from side effects.
+
+    """
+
+    @property
+    def rltime(self):
+        """Returns the current tracker"""
+        return globals()["__rltracker__"]
+
+    @rltime.setter
+    def rltime(self, val):
+        """Sets the current tracker"""
+        globals()["__rltracker__"] = val
+
+    def ratelimitcheck(self, amount_allow=9, within_time=1):
+        """Checks if WeThePeople needs pause to prevent api banning"""
+
+        if len(self.rltime) >= amount_allow:
+            currenttime = timestamp()
+            try:
+                while (self.rltime[-1]+within_time) < currenttime:
+                    del self.rltime[-1]
+                if len(self.rltime) >= amount_allow:
+                    return False
+            except IndexError as err:
+                return len(self.rltime) == 0
+
+            else:
+                return True
+        else:
+            return True
+
+    def add_timestamp(self):
+        """Adds timestamp to rltime"""
+        self.rltime = [timestamp()] + self.rltime
+
+
+class SessionWrapper(RateLimit):
 
     """
     This Class Wraps around Request.Session
@@ -33,10 +83,15 @@ class SessionWrapper(object):
         self.session = Session()
 
     def get(self, url):
-        return self.session.get(url, verify=True)
+        while not self.ratelimitcheck():
+            time.sleep(0.1)
+        response = self.session.get(url, verify=True)
+        self.add_timestamp()
+        print(self.rltime)
+        return response
 
-    def post(self):
-        raise NotImplementedError
+    def post(self, url, params):
+        return self.session.POST(url, params=params, verify=True)
 
 
 class RequestObject(object):
